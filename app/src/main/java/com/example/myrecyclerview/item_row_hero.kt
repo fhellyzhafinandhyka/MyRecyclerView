d6d878dfc5cd1b7b53467183a07350dc471bee0d
Fhellyzha Finandhyka <finandhykafhellyzha@gmail.com>
2024-03-19 22:18:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myrecyclerview/item_row_hero.kt
@@ -0,0 +1,10 @@
+package com.example.myrecyclerview
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class item_row_hero : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_row_hero)
+    }
+}
